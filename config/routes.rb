199f950b2e3c8f8afdebeb3d0028fa2a07897938
Michael Desa <mjdesa@gmail.com>
2012-02-29 03:20:22 -0800

    Basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Printless::Application.routes.draw do
+  get "users/new"
+  
+  match '/signup', to: 'users#new'
+
   root :to => 'temp_homepage#temphome'
 end
--- spec/requests/temp_homepages_spec.rb
+++ spec/requests/temp_homepages_spec.rb
@@ -5,12 +5,12 @@ describe "TempHomepages" do
   describe "Tempoarary Home" do
     
     it "should have the content 'Temporary Homepage'" do
-      visit '/'
+      visit root_path
       page.should have_content('Temporary Homepage')
     end
     
     it "should have the correct title" do
-      visit '/'
+      visit root_path
       page.should have_selector('title', text: "Printless")
     end    
   end
