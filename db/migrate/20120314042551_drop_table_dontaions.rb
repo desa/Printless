41d5fe7d102fc348e218b56f1e9d0d2c27b59fc1
Michael Desa <mjdesa@gmail.com>
2012-03-17 09:12:42 -0700

    work work

--- db/migrate/20120314042551_drop_table_dontaions.rb
+++
@@ -1,5 +0,0 @@
-class DropTableDontaions < ActiveRecord::Migration
-  def change
-    drop_table(:dontaions)
-  end
-end
--- db/migrate/20120314042845_drop_table_dontaions2.rb
+++
@@ -1,5 +0,0 @@
-class DropTableDontaions2 < ActiveRecord::Migration
-  def change
-    drop_table(:dontaions)
-  end
-end
