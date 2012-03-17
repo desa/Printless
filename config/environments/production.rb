11979b2a09ed08f464a717a23ad97b5a35a58b44
Michael Desa <mjdesa@gmail.com>
2012-03-17 09:49:27 -0700

    okay

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Printless::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  ####### Note that I changed this ########
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
